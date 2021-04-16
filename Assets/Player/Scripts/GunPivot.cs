54edeb0b1b48f5a92e742f5485157a5662d1a04e
Shady Fanous <shady@synapse-analytics.io>
2021-04-16 16:39:24 +0200

    wip(Laser Gun): Attempting to make gun follow the player's mouse

---
+++ Assets/Player/Scripts/GunPivot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Player.Scripts {
+    public class GunPivot : MonoBehaviour {
+        // Update is called once per frame
+        void FixedUpdate() {
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            difference.Normalize();
+            float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, rotationZ);
+            if (rotationZ < -90 || rotationZ > 90) {
+                
+            }
+        }
+    }
+}
