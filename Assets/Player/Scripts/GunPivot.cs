22da664dd297c33616171dcb8072011b7c4cc9d6
Shady Fanous <shady@synapse-analytics.io>
2021-04-29 16:21:56 +0200

    fix(GunPivot): Made GunPivot have a rotation offset based on the gun's fire point

--- Assets/Player/Scripts/GunPivot.cs
+++ Assets/Player/Scripts/GunPivot.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using Utils;
 
-// TODO: Make clamping a variable
-// TODO: Make firepoint follow mouse instead of pivot
 // https://www.youtube.com/watch?v=6hp9-mslbzI
 namespace Player.Scripts {
     public class GunPivot : MonoBehaviour {
         private GameObject _player;
+        private float _rotOffset;
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
         private void Awake() {
+            Transform objTransform = transform;
+
+            // Get the player's transform, needed to figure out which direction the player is facing
             try {
-                _player = transform.parent.gameObject;
+                _player = objTransform.parent.gameObject;
             }
             catch (Exception) {
                 Logger.Error("An error occured when trying to find the parenting Player object of the GunPivot");
@@ -22,6 +24,27 @@ namespace Player.Scripts {
 
             Assert.IsTrue(_player.CompareTag("Player"),
                 "Gun Pivot object must be the direct child of an object with the tag \"Player\"");
+
+            // Get the FirePoint's transform, needed to calculate the rotation offset of the GunPivot
+            Transform gunTransform;
+            try {
+                gunTransform = objTransform.GetChild(0);
+            }
+            catch (Exception) {
+                Logger.Error("GunPivot couldn't find the gun attached to it");
+                throw;
+            }
+
+            var firePoint = gunTransform.Find("FirePoint");
+            if (!firePoint) {
+                throw new NullReferenceException(
+                    "GunPivot couldn't find the \"FirePoint\" Transform of its attached gun");
+            }
+
+            // Find the angle between the fire point and the gun pivot and use that as an offset
+            Vector3 dif = (firePoint.position - objTransform.position);
+            dif.Normalize();
+            _rotOffset = Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg;
         }
 
         // Read this https://docs.unity3d.com/ScriptReference/Quaternion-eulerAngles.html
@@ -35,10 +58,9 @@ namespace Player.Scripts {
             float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
             rotationZ = (rotationZ + 360) % 360;
 
-            Logger.Debug(rotationZ);
-
             // If the player is facing right
             if (_player.transform.eulerAngles.y == 0f) {
+                rotationZ -= _rotOffset;
                 // Clamp weapon at top, check if mouse in Q2
                 if (rotationZ > 90f && rotationZ < 180f) {
                     transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
@@ -54,6 +76,7 @@ namespace Player.Scripts {
             }
             // if player is facing left
             else if (Mathf.Abs(_player.transform.eulerAngles.y) == 180f) {
+                rotationZ += _rotOffset;
                 // Clamp weapon at top, check if mouse is in Q1
                 if (rotationZ < 90f && rotationZ > 0f) {
                     transform.localRotation = Quaternion.Euler(180f, 180f, -90f);
