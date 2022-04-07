bae570e75e2994bfc8371da882817e1361f90312
Shady Fanous <shady@synapse-analytics.io>
2022-04-07 21:34:45 +0200

    chore: update .editorconfig

--- Assets/Weapons/LaserGun.cs
+++ Assets/Weapons/LaserGun.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserGun : MonoBehaviour
-{
+public class LaserGun : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
     }
 }
