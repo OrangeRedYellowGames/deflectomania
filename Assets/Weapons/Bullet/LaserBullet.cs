cfd471c503f5d1145c9442b7892434ab64a2c40f
ShadyF <shadster09@gmail.com>
2022-05-07 15:58:09 +0200

    wip: make laser bullets reflect off walls

--- Assets/Weapons/Bullet/LaserBullet.cs
+++ Assets/Weapons/Bullet/LaserBullet.cs
@@ -1,6 +1,6 @@
-using System;
-using System.Net;
+using NLog;
 using UnityEngine;
+using Logger = NLog.Logger;
 
 namespace Weapons.Bullet {
     [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
@@ -8,6 +8,15 @@ namespace Weapons.Bullet {
         public int speed = 20;
         public int numOfReflections = 3;
         private Rigidbody2D _rb;
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private void OnDrawGizmos() {
+            var normalizedVelocityVector = _rb.velocity.normalized;
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position,
+                new Vector3(normalizedVelocityVector.x, normalizedVelocityVector.y, 0) * 2 + transform.position);
+        }
 
         void Awake() {
             _rb = GetComponent<Rigidbody2D>();
@@ -17,8 +26,30 @@ namespace Weapons.Bullet {
             _rb.velocity = transform.right * speed;
         }
 
-        private void OnTriggerEnter2D(Collider2D col) {
-            Destroy(gameObject);
+        private void OnCollisionEnter2D(Collision2D collision) {
+            // Destroy the bullet if the reflection count reached 0
+            if (numOfReflections == 0) {
+                Destroy(gameObject);
+            }
+
+            if (collision.contactCount > 1) {
+                Logger.Warn(
+                    $"Bullet collided with {collision.contactCount.ToString()} contact points. Using contact point at index 0 to calculate the reflection angle.");
+            }
+
+            // Get the contact point at index 0
+            ContactPoint2D contact = collision.GetContact(0);
+
+            // Cannot use _rb.velocity here since it will be zero. This is because OnCollisionEnter2D gets called after FixedUpdate gets called
+            // where the _rb.velocity will be zero because a collision occured. See https://forum.unity.com/threads/vector3-reflection-not-working-as-it-should.517743/
+            // for more details
+            var newDirection = Vector3.Reflect(-collision.relativeVelocity, contact.normal);
+
+            transform.right = newDirection;
+            _rb.velocity = transform.right * speed;
+
+            // Decrement the number of reflections variable
+            numOfReflections--;
         }
     }
 }
