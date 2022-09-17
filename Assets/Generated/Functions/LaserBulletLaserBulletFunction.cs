4bdb685c545fa65b33f25ecabf64d972aaf1a767
Shady Fanous <shadster09@gmail.com>
2022-09-17 13:23:05 +0200

    refactor: remove unused unity atoms function

--- Assets/Generated/Functions/LaserBulletLaserBulletFunction.cs
+++
@@ -1,9 +0,0 @@
-using Weapons.Bullet;
-namespace UnityAtoms.BaseAtoms
-{
-    /// <summary>
-    /// Function x 2 of type `Weapons.Bullet.LaserBullet`. Inherits from `AtomFunction&lt;Weapons.Bullet.LaserBullet, Weapons.Bullet.LaserBullet&gt;`.
-    /// </summary>
-    [EditorIcon("atom-icon-sand")]
-    public abstract class LaserBulletLaserBulletFunction : AtomFunction<Weapons.Bullet.LaserBullet, Weapons.Bullet.LaserBullet> { }
-}
