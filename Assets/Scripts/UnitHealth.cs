51dd66dcfa666ddbeb3f9dbcbbec06a5a1589a79
Amr Morsey <amr.morsey@aucegypt.edu>
2022-11-26 22:08:44 +0200

    feat: Win System - Add int value lists that keeps track of alive and current players. - Add logic to disallow player to rejoin after dying. - Add winEvent to be triggered when a player wins. - Add logic to check for win condition.

--- Assets/Scripts/UnitHealth.cs
+++ Assets/Scripts/UnitHealth.cs
@@ -3,9 +3,8 @@ using UnityEngine;
 using UnityEngine.Assertions;
 
 /// <summary>
-/// Modular component that should be attached to any game object that requires "health".
-///
-/// Should be used in tandem with the DamageDealer component in order to inflict damage.
+///     Modular component that should be attached to any game object that requires "health".
+///     Should be used in tandem with the DamageDealer component in order to inflict damage.
 /// </summary>
 public class UnitHealth : MonoBehaviour {
     [Tooltip("Variable that holds the HP of the object.")]
@@ -15,24 +14,21 @@ public class UnitHealth : MonoBehaviour {
     public IntEvent deathEvent;
 
     /// <summary>
-    /// Optional. Player ID that will be raised with the death event.
+    ///     Optional. Player ID that will be raised with the death event.
     /// </summary>
     public IntReference playerId;
 
-    void Awake() {
+    private void Awake() {
         Assert.IsNotNull(hp, "HP Variable can't be missing in UnitHealth");
     }
 
     /// <summary>
-    /// Use this function to inflict damage onto the attached object.
-    ///
-    /// If HP reaches 0, deathEvent will be raised.
+    ///     Use this function to inflict damage onto the attached object.
+    ///     If HP reaches 0, deathEvent will be raised.
     /// </summary>
     /// <param name="damage">Amount of damage to take</param>
     public void TakeDamage(float damage) {
         hp.Value -= damage;
-        if (hp.Value <= 0 && deathEvent) {
-            deathEvent.Raise(playerId);
-        }
+        if (hp.Value <= 0 && deathEvent) deathEvent.Raise(playerId);
     }
 }
---
+++ Assets/Scripts/WinSystem.cs
@@ -0,0 +1,67 @@
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class WinSystem : MonoBehaviour {
+    public IntValueList alivePlayers;
+    public IntEvent playerDeathEvent;
+    public IntEvent playerWinEvent;
+    private bool _recentlyDeceasedPlayer;
+
+    /// <summary>
+    ///     On awake it asserts that all the required events are there.
+    /// </summary>
+    private void Awake() {
+        Assert.IsNotNull(alivePlayers);
+        Assert.IsNotNull(playerDeathEvent);
+        Assert.IsNotNull(playerWinEvent);
+
+        playerDeathEvent.Register(OnPlayerDeath);
+    }
+
+    /// <summary>
+    ///     On start it invokes a repeating function every 0.5f seconds to check win condition.
+    /// </summary>
+    private void Start() {
+        _recentlyDeceasedPlayer = false;
+        InvokeRepeating(nameof(CheckWinCondition), 0, 0.5f);
+    }
+
+    /// <summary>
+    ///     This function is unregisters the playerDeathEvent when this object is destroyed.
+    /// </summary>
+    private void OnDestroy() {
+        playerDeathEvent.Unregister(OnPlayerDeath);
+    }
+
+    /// <summary>
+    ///     Function that gets called every 0.5f seconds to check if there is a win condition. It checks if a player
+    ///     has died recently and if yes goes a head and computes the winner if any.
+    /// </summary>
+    private void CheckWinCondition() {
+        if (!_recentlyDeceasedPlayer)
+            return;
+
+        if (alivePlayers.Count == 1) {
+            Debug.Log("Winner! is Player " + alivePlayers[0]);
+            playerWinEvent.Raise(alivePlayers[0]);
+        }
+        else {
+            if (alivePlayers.Count == 0) {
+                Debug.Log("Draw!");
+                // -1 denotes a draw!
+                playerWinEvent.Raise(-1);
+            }
+        }
+
+        _recentlyDeceasedPlayer = false;
+    }
+
+    /// <summary>
+    ///     This function sets a flag that a player has died recently.
+    /// </summary>
+    /// <param name="playerIndex"></param>
+    private void OnPlayerDeath(int playerIndex) {
+        _recentlyDeceasedPlayer = true;
+    }
+}
